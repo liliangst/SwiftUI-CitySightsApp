29d26a0e580318f5e53aea48c58e084caa2debd6
Lilian Grasset <lilian.grasset@icloud.com>
2021-08-22 18:28:44 +0200

    - Set up folders for views models and view models

--- City Sights App/City_Sights_AppApp.swift
+++ City Sights App/City_Sights_AppApp.swift
@@ -1,5 +1,5 @@
 //
-//  City_Sights_AppApp.swift
+//  City_Sights_App.swift
 //  City Sights App
 //
 //  Created by Lilian Grasset on 22/08/2021.
@@ -8,10 +8,10 @@
 import SwiftUI
 
 @main
-struct City_Sights_AppApp: App {
+struct City_Sights_App: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            LaunchView()
         }
     }
 }
