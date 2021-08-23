71487eb371f59ebac41c9912fffdbef604e15aaf
Lilian Grasset <lilian.grasset@icloud.com>
2021-08-23 15:17:54 +0200

    - renaming files - location authorization

--- City Sights App/CitySightsApp.swift
+++ City Sights App/CitySightsApp.swift
@@ -1,5 +1,5 @@
 //
-//  City_Sights_App.swift
+//  CitySightsApp.swift
 //  City Sights App
 //
 //  Created by Lilian Grasset on 22/08/2021.
@@ -8,10 +8,11 @@
 import SwiftUI
 
 @main
-struct City_Sights_App: App {
+struct CitySightsApp: App {
     var body: some Scene {
         WindowGroup {
             LaunchView()
+                .environmentObject(ContentModel())
         }
     }
 }
